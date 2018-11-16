a5a4858b9b273d4554f795f98659aae528eb3c24
Azmir Shah <azmirshah1@gmail.com>
2018-11-16 17:10:47 +0800

    Update 1.1
    Testing on input dan output process

--- sekolah/uum.py
+++ sekolah/uum.py
@@ -1,1 +1,7 @@
-print("Hello World\n World will End")
+character_name = "john"
+character_age = 35
+print("He is " + character_name.upper() + " and he is " + str(character_age) + " years old")
+
+name = input("Tell me ur name? -->")
+age = input("ur age? -->")
+print("Hai " + name + "\nWow, you are " + age + " years old.")
